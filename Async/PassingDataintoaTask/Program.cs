02908dcf5da4fd3ed61a99ac56cdb6f364501177
trendfollowerpl <trendfollowerpl@windowslive.com>
2018-04-27 19:23:19 +0200

    PassingDataintoaTask

---
+++ Async/PassingDataintoaTask/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PassingDataintoaTask
+{
+	public class DataImporter
+	{
+		public void Import(string directory)
+		{
+			// Import files from this.directory
+		}
+	}
+
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			var importer = new DataImporter();
+			string importDirectory = @"C:\data";
+			Task.Factory.StartNew(() => importer.Import(importDirectory));
+		}
+	}
+}
