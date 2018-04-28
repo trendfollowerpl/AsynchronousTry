381c016dd0e02743b4ca47e6a1b40b560089aeb4
trendfollowerpl <trendfollowerpl@windowslive.com>
2018-04-28 19:00:05 +0200

    ErrorHandling 1

---
+++ Async/ErrorHandling/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ErrorHandling
+{
+	class Program
+	{
+		static async Task Main(string[] args)
+		{
+			Task task = Task.Factory.StartNew(() => throw new Exception("HAHA"));
+			try
+			{
+				await task;
+			}
+			catch (Exception ex)
+			{
+				Console.Write(ex.Message);
+			}
+			Console.ReadLine();
+		}
+	}
+}
