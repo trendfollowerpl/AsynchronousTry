90ff60f50064470cf183923bbac803294e79faf6
trendfollowerpl <trendfollowerpl@windowslive.com>
2018-05-08 19:43:55 +0200

    Chaining_Tasks_Continuations setup

---
+++ Async/Chaining Tasks Continuations/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Chaining_Tasks_Continuations
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			Task<int> firstTask =
+				Task.Factory.StartNew<int>(
+					() =>
+					{
+						Console.WriteLine("First Task");
+						return 42;
+					});
+			Task secondTask =
+				firstTask.ContinueWith(
+					ftask => Console.WriteLine($"Second Task, First task returned {ftask.Result}")
+					);
+
+			
+
+		}
+	}
+}
