7a35c137be8c543104cc17e2b4f9b95049815f8a
trendfollowerpl <trendfollowerpl@windowslive.com>
2018-05-08 19:47:18 +0200

    moved project to .nert 4,7,1

--- Async/Chaining Tasks Continuations/Program.cs
+++ Async/Chaining Tasks Continuations/Program.cs
@@ -8,7 +8,7 @@ namespace Chaining_Tasks_Continuations
 {
 	class Program
 	{
-		static void Main(string[] args)
+		static async Task Main(string[] args)
 		{
 			Task<int> firstTask =
 				Task.Factory.StartNew<int>(
@@ -21,8 +21,11 @@ namespace Chaining_Tasks_Continuations
 				firstTask.ContinueWith(
 					ftask => Console.WriteLine($"Second Task, First task returned {ftask.Result}")
 					);
+			await secondTask;
 
-			
+			Console.WriteLine($"firstTask Status: {firstTask.Status}");
+			Console.WriteLine($"secondTask Status: {secondTask.Status}");
+			Console.ReadLine();
 
 		}
 	}
