dff583bf87ef33258e37497136068f34370c3896
trendfollowerpl <trendfollowerpl@windowslive.com>
2018-05-14 21:00:15 +0200

    examples

--- Async/Nested and Child Tasks/Program.cs
+++ Async/Nested and Child Tasks/Program.cs
@@ -8,8 +8,29 @@ namespace Nested_and_Child_Tasks
 {
 	class Program
 	{
-		static void Main(string[] args)
-		{
+		static async Task Main(string[] args)
+		{   //does not throw error of inner task
+			var Task1 = Task.Factory.StartNew(() =>
+				Task.Factory.StartNew((() =>
+				{
+					Console.WriteLine("Nested..");
+					throw new Exception("Nested exception Task 1");
+				}))
+				, TaskCreationOptions.DenyChildAttach);
+
+			//inner error affect outer task
+			var Task2 = Task.Factory.StartNew(() =>
+				Task.Factory.StartNew(() =>
+				{
+					Console.WriteLine("Nested..");
+					throw new Exception("Nested exception Task 2");
+				}, TaskCreationOptions.AttachedToParent));
+
+			await Task1;
+			await Task2;
+
+
+			Console.ReadLine();
 		}
 	}
 }
