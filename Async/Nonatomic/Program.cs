cbee255bc8e7be88187808e1acda8051cbffc6e6
trendfollowerpl <trendfollowerpl@windowslive.com>
2018-05-15 20:00:11 +0200

    nonatomic example

--- Async/Nonatomic/Program.cs
+++ Async/Nonatomic/Program.cs
@@ -8,8 +8,32 @@ namespace Nonatomic
 {
 	class Program
 	{
-		static void Main(string[] args)
+		static async Task Main(string[] args)
 		{
+			const int iterations = 100000000;
+			const int numTasks = 2;
+			var tasks = new List<Task>();
+			int value = 0;
+
+			Enumerable.Range(0, numTasks).ToList().ForEach(notused =>
+			{
+				tasks.Add(Task.Factory.StartNew(() =>
+				{
+					IncrementValue(ref value, iterations);
+				}));
+			});
+
+			Task.WaitAll(tasks.ToArray());
+			Console.WriteLine("Expected value: {0}, Actual value: {1}", numTasks * iterations, value);
+			Console.ReadLine();
+		}
+
+		private static void IncrementValue(ref int value, int iterations)
+		{
+			for (int i = 0; i < iterations; i++)
+			{
+				value++;
+			}
 		}
 	}
 }
