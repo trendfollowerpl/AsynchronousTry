168861d31011f175dc20f4654dc575627c43d73a
trendfollowerpl <trendfollowerpl@windowslive.com>
2018-05-16 19:30:20 +0200

    Monitor proj added

---
+++ Async/Monitor/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Monitor
+{
+	public class SmallBusiness
+	{
+		private decimal cash;
+		private decimal receivables;
+		public SmallBusiness(decimal cash, decimal receivables)
+		{
+			this.cash = cash;
+			this.receivables = receivables;
+		}
+		public void ReceivePayment(decimal amount)
+		{
+			cash += amount;
+			receivables -= amount;
+		}
+		public decimal NetWorth
+		{
+			get { return cash + receivables; }
+		}
+	}
+
+	class Program
+	{
+		static void Main(string[] args)
+		{
+		}
+	}
+}
--- Async/The Interlocked Class/Program.cs
+++ Async/The Interlocked Class/Program.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 
 namespace The_Interlocked_Class
@@ -10,6 +11,30 @@ namespace The_Interlocked_Class
 	{
 		static void Main(string[] args)
 		{
+			const int iterations = 100000000;
+			const int numTasks = 2;
+			var tasks = new List<Task>();
+			int value = 0;
+
+			Enumerable.Range(0, numTasks).ToList().ForEach(notused =>
+			{
+				tasks.Add(Task.Factory.StartNew(() =>
+				{
+					IncrementValue(ref value, iterations);
+				}));
+			});
+
+			Task.WaitAll(tasks.ToArray());
+			Console.WriteLine("Expected value: {0}, Actual value: {1}", numTasks * iterations, value);
+			Console.ReadLine();
+		}
+
+		private static void IncrementValue(ref int value, int iterations)
+		{
+			for (int i = 0; i < iterations; i++)
+			{
+				Interlocked.Increment(ref value);
+			}
 		}
 	}
 }
